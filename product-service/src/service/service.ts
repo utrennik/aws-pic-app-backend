920e0bca6f73d79cf0b63fa37ea7c0f09b1b3fb5
Utrennik <utrennik@list.ru>
2022-06-26 23:54:03 +0300

    feat: add db service

--- product-service/src/service/service.ts
+++ product-service/src/service/service.ts
@@ -1,16 +1,53 @@
+import { Client } from 'pg';
 import { IProduct } from './../models/types';
 import { NotFoundError } from '../errors/notFoundError';
+import { ConnectionError } from '../errors/connectionError';
 import products from '../mocks/products';
 
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+
+const dbOptions = {
+  host: PG_HOST,
+  port: PG_PORT,
+  database: PG_DATABASE,
+  user: PG_USERNAME,
+  password: PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeoutMillis: 5000,
+};
+
+let client;
+
+const connect = async () => {
+  try {
+    client = new Client(dbOptions);
+    await client.connect();
+  } catch (error) {
+    throw new ConnectionError(error.message);
+  }
+};
+
 const getAll = async () => {
-  return products;
+  if (!client) await connect();
+  const { rows: products } = await client.query('SELECT * FROM products');
+  const { rows: stocks } = await client.query('SELECT * FROM stocks');
+  const result = products.map((product: IProduct) => {
+    const stock = stocks.find((stock: any) => stock.product_id === product.id);
+    return { ...product, count: stock.count };
+  });
+  return result;
 };
 
 const getById = async (id: string) => {
-  const product: IProduct = products.find((product: IProduct) => product.id === Number(id));
-  if (!product) {
-    throw new NotFoundError(`Product with id ${id} not found`);
-  }
+  if (!client) await connect();
+  const { rows: products } = await client.query('SELECT * FROM products WHERE id = $1', [id]);
+  if (!products.length) throw new NotFoundError(`Product with id ${id} not found`);
+  const { rows: stocks } = await client.query('SELECT * FROM stocks WHERE product_id = $1', [id]);
+  if (!stocks.length) throw new NotFoundError(`Stock info for product with id ${id} not found`);
+  const product = { ...products[0], count: stocks[0].count };
+
   return product;
 };
 
