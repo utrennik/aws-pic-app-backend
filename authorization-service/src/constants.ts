3ed31a46e0f0c046b332f8a55dc8b91d69b76dc1
Utrennik <32798960+utrennik@users.noreply.github.com>
2022-07-29 18:58:15 +0300

    Task 7 (#6)
    * feat: implement cognito auth

---
+++ authorization-service/src/constants.ts
@@ -0,0 +1,6 @@
+export const ALLOW = 'Allow';
+export const DENY = 'Deny';
+export const UNAUTHORIZED = 'Unauthorized';
+export const TOKEN = 'TOKEN';
+export const TOKEN_ERROR_MESSAGE = 'No authorization token found in the request or invalid token';
+export const INVALID_CREDENTIALS_MESSAGE = 'Invalid credentials. Please check your username and password';
